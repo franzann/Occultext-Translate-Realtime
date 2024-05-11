97ea1d7635575c363005543cb48d9ae5d8310cd0
franzann <166019694+franzann@users.noreply.github.com>
2024-05-11 20:43:08 +0800

    Add files via upload

--- background.js
+++ background.js
@@ -1,14 +1,30 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if (request.action == "translateWord") {
-    fetch('https://frenzvalios.com/api/occultext/decrypt/atlas?occultext=' + encodeURIComponent(request.word))
-      .then(response => response.json())
-      .then(data => {
-        sendResponse({ original: data.original_word });
-      })
-      .catch(error => {
-        console.error("error:", error);
-        sendResponse({});
-      });
-    return true;
+    const word = request.word;
+
+    chrome.storage.local.get(word, function(result) {
+      if (result.hasOwnProperty(word)) {
+        sendResponse({ original: result[word] });
+      } else {
+        fetch('https://frenzvalios.com/api/occultext/decrypt/atlas?occultext=' + encodeURIComponent(word))
+          .then(response => response.json())
+          .then(data => {
+            if (data && data.original_word) {
+              let storeObject = {};
+              storeObject[word] = data.original_word;
+              chrome.storage.local.set(storeObject, function() {
+                sendResponse({ original: data.original_word });
+              });
+            } else {
+              sendResponse({});
+            }
+          })
+          .catch(error => {
+            console.error("error:", error);
+            sendResponse({});
+          });
+      }
+    });
+    return true; 
   }
 });
